ae5bbeba2ad788e3c161ff0f5761070b0ff48419
Polibin Alexei <polibin.alexei@gmail.com>
2023-10-12 19:58:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dev</title>
+</head>
+<body>
+	<h1>Dev</h1>
+	<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Sapiente rem pariatur eligendi nulla minus libero et, cupiditate suscipit non eaque.</p>
+</body>
+</html>
