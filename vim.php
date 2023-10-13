e91e706378ea28f2f1e2b570f04f0e998b9f194a
Polibin Alexei <polibin.alexei@gmail.com>
2023-10-13 12:46:54 +0300

    Add vim.

---
+++ vim.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Vim</title>
+</head>
+<body>
+	<h1>Vim</h1>
+	<p>k - up.</p>
+	<p>l - right.</p>
+	<p>j - down.</p>
+	<p>h - left.</p>
+	<p>:q! - exit without save.</p>
+	<p>x - delete the character.</p>
+	<p>i - to insert text.</p>
+	<p>A - to append text.</p>
+</body>
+</html>
